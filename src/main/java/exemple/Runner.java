63d9d48c7ee2cb9091d6ab8095e3aa4291302ee7
Maksym_Moroz1 <maximussmoroz@gmail.com>
2017-09-14 16:45:22 +0300

    third

--- src/main/java/exemple/Runner.java
+++ src/main/java/exemple/Runner.java
@@ -8,11 +8,11 @@ import java.util.Collection;
 public class Runner {
     public static void main(String[] args) {
         CatalogXMLBuilder catalogXMLBuilder = new CatalogXMLBuilder();
-        catalogXMLBuilder.addperson("Vasya1", "Damask", "12000", "Hight");
-        catalogXMLBuilder.addperson("Vasya2", "Bagdad", "100000", "Hight");
-        catalogXMLBuilder.addperson("Vasya3", "Tripoly", "1001100", "Hight");
-        catalogXMLBuilder.addperson("Vasya4", "Tegeran", "10000", "Hight");
-        catalogXMLBuilder.addperson("Vasya5", "Cairo", "201100", "Hight");
+        catalogXMLBuilder.addperson("Vasya1", "Damask", "1200", "Hight");
+        catalogXMLBuilder.addperson("Vasya2", "Bagdad", "10000", "Hight");
+        catalogXMLBuilder.addperson("Vasya3", "Tripoly", "101100", "Hight");
+        catalogXMLBuilder.addperson("Vasya4", "Tegeran", "1000", "Hight");
+        catalogXMLBuilder.addperson("Vasya5", "Cairo", "21100", "Hight");
         Writer writer = new Writer();
 
         System.out.println(writer.toStringXMLRepresentation(catalogXMLBuilder.getDocument()));
@@ -21,7 +21,7 @@ public class Runner {
                 "C:\\Users\\Maksym_Moroz1\\Desktop\\text.txt");
 
         PersonParser personParser = new PersonParser("C:\\Users\\Maksym_Moroz1\\Desktop\\text.txt");
-        Collection<Person> persons = personParser.getRangedPersons(100000);
+        Collection<Person> persons = personParser.getRangedPersons(10000);
 
         writer.extructPersonsCollectionToFile(persons,
                 "C:\\Users\\Maksym_Moroz1\\Desktop\\text1.txt");
